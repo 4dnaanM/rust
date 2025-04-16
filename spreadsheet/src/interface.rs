1bec1b3665aaba596c190e26a65addc42b841a49
Siddharth Gupta <97736388+sidg2110@users.noreply.github.com>
2025-04-16 14:36:22 +0530

    Interface with backend complete

--- spreadsheet/src/interface.rs
+++ spreadsheet/src/interface.rs
@@ -1,10 +1,16 @@
+use crate::equation::Equation;
 use crate::parser;
+use crate::parser::cell::Value;
 use crate::parser::command::Command;
 use crate::parser::error::Error;
 use crate::parser::print_output::print_sheet;
 use crate::spreadsheet::SpreadSheet;
 use std::time::Instant;
 use std::io::{self, Write};
+use crate::utils::Type;
+use crate::utils::Coordinate;
+use crate::operand::SharedOperand;
+use crate::operand::Operand;
 
 pub fn process_command(user_input: &str, spreadsheet: &mut SpreadSheet, row: &mut usize, col: &mut usize, enable_output: &mut bool, quit: &mut bool, max_rows: &usize, max_cols: &usize) {
     let start = Instant::now();
@@ -17,10 +23,67 @@ pub fn process_command(user_input: &str, spreadsheet: &mut SpreadSheet, row: &mu
     };
     match command {
         Command::RangeCommand(cmd) => {
+            let Value::Cell(cell) = cmd.target_cell else {
+                panic!();  
+            };
+            let Value::Cell(operand_1) = cmd.operand_1 else {
+                panic!();
+            };
+            let Value::Cell(operand_2) = cmd.operand_2 else {
+                panic!();
+            };
+            let t = Some(Type::from_str(cmd.function.as_str()));
+            let operands = Some(vec![spreadsheet.cells[operand_1.row-1][operand_1.col-1].clone(), spreadsheet.cells[operand_2.row-1][operand_2.col-1].clone() ]);
+            let equation = Equation::new(Coordinate(cell.row-1, cell.col-1), t, operands);
+            spreadsheet.set_cell_equation(cell.row-1, cell.col-1, equation);
             
+            if *enable_output {
+                *row = cell.row;
+                *col = cell.col;    
+                print_sheet(*row , *col, spreadsheet, *max_rows, *max_cols);
+            }
         },
         Command::ArithmeticCommand(cmd) => {
-            
+            let Value::Cell(cell) = cmd.target_cell else {
+                panic!();  
+            };
+
+            let operand_1 = match cmd.operand_1 {
+                Value::Cell(cell_1) => {
+                    spreadsheet.cells[cell_1.row-1][cell_1.col-1].clone()
+                },
+                Value::Constant(constant) => {
+                    SharedOperand::new(Operand::new(Some((0, 1)), Some(constant)))
+                }
+            };
+
+            let operand_2 = match cmd.operand_2 {
+                Some(operand_2) => match operand_2 {
+                    Value::Cell(cell_2) => {
+                        spreadsheet.cells[cell_2.row-1][cell_2.col-1].clone()
+                    },
+                    Value::Constant(constant) => {
+                        SharedOperand::new(Operand::new(Some((0, 1)), Some(constant)))
+                    }
+                },
+                None => SharedOperand::new(Operand::new(Some((0, 1)), Some(0)))
+            };
+
+            let t = match cmd.operator {
+                Some(op) => Some(Type::from_str(op.as_str())),
+                None => Some(Type::from_str("+"))
+            };
+
+            let operands = Some(vec![operand_1, operand_2]);
+            let equation = Equation::new(Coordinate(cell.row-1, cell.col-1), t, operands);
+            spreadsheet.set_cell_equation(cell.row-1, cell.col-1, equation);
+
+            if *enable_output {
+                *row = cell.row;
+                *col = cell.col;    
+                print_sheet(*row , *col, spreadsheet, *max_rows, *max_cols);
+            }
+
         },
         Command::UserInteractionCommand(cmd) => {
             let ui_command = cmd.command.clone();
--- spreadsheet/src/spreadsheet.rs
+++ spreadsheet/src/spreadsheet.rs
@@ -19,6 +19,7 @@ impl SpreadSheet {
         for i in 0..m {
             let mut row = Vec::<SharedOperand>::with_capacity(n);
             for j in 0..n {
+                // row.push(SharedOperand::new(Operand::new(Some((i, j)), Some((i+j) as i32))));
                 row.push(SharedOperand::new(Operand::new(Some((i, j)), Some(0))));
             }
             cells.push(row);
